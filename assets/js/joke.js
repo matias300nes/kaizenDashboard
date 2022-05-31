31cd882857cf0c4660a5068db71c87ac6773c504
matias endres <55670432+matias300nes@users.noreply.github.com>
2022-05-31 01:49:35 +0000

    Initial commit

---
+++ assets/js/joke.js
@@ -0,0 +1,31 @@
+function checkJoke(){
+    fetch('https://flasktest.matiasendres.repl.co/isJoke')
+    .then(response => response.json())
+    .then(data => {
+        if (data.joke == true){
+            alert("JOKE!!")
+            sendJoke(false)
+        }
+    });
+    
+    setTimeout(checkJoke, 2000);
+    
+}
+
+function sendJoke(state){
+    var url = 'https://flasktest.matiasendres.repl.co/setJoke';
+    var data = {joke: state};
+    
+    fetch(url, {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+    
+}
+
+checkJoke();
