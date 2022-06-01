6efb3fc9c98264cd3c42396559348078f26910e6
Tomas Alfonso <66142185+tomasalfonsook@users.noreply.github.com>
2022-06-01 17:16:38 +0000

    Subiendo todos los cambios de nuevo

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
  // sending a connect request to the server.
- var socket = io.connect('http://localhost:5000');
+ var socket = io.connect('https://flasktest.matiasendres.repl.co:5000');
 });
+
+
+// An event handler for a change of value 
+$('input.sync').on('input', function(e) {
+    socket.emit('message', {
+        who:$(this).attr('id'),  
+        data: $(this).val()
+     });
+     return false;
+ });
